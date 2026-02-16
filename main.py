91e8590d765f578c2cf6dde32d1da7f6749f2134
savi4m83-crypto <savi4m83@gmail.com>
2026-02-15 16:17:38 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Ram is congratulating his friend for getting a gold medal in the state games.
+# he wrote the "congratulation in lower case while giving input and now wants to
+# convert it in the upper case. Write the program for converting the word in
+# upper case.
+print("CONGRATULATIONS")
